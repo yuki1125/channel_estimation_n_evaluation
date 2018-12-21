c4170b12649419678dc9ee53566741c961da73ee
yuki1125 <41186144+yuki1125@users.noreply.github.com>
2018-12-21 10:50:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,14 +14,22 @@ boxNoise = [0.0, 0.2, 0.3, 0.4]
 print('Processing is about to begin')
 
 for i, _boxNoise in enumerate(boxNoise): 
+    # 画像の作成
     mri = MakeReceivedImg(numberOfLED=_num_led, gaussSigma=_gaussSigma, boxNoise=_boxNoise, \
                         kernelSize=_kernelSize, maxLum=_maxLum, offset=False)
+    # loop枚数分画像の作成
+    # - answer_signals: 正解の点滅パターン
+    # - pixel_values: 受信画素値（ノイズ＋ガウスフィルタ)
     answer_signals, pixel_values = mri.create_dataset(loop=_loop)
 
+    # - ch_pro: 擬似逆行列を用いたチャネル推定
     ch_pro = chest.estimate_channel(answer_signals, pixel_values, offset=False)
+    # - ch_conv: 単独点滅画像を用いたチャネル推定
     ch_conv = chest.estimate_channel_conv(gaussSigma=_gaussSigma, kernelSize=_kernelSize)
 
+    # - replicas_with_pro: ch_proを用いたレプリカ画像作成
     replicas_with_pro = md.create_replica(numleds=_num_led, ch_pro)
+    # - replicas_with_conv: ch_convを用いたレプリカ画像作成
     replicas_with_conv = md.create_replica(ch_conv)
 
     print('now processing....:', i, '/', len(boxNoise))
